9befa90337c1fa7228959bfd6e66a530bba96a25
Orest-Lemchyk <orestlema@gmail.com>
2025-10-27 16:46:55 +0200

    rebild to backend CRUD

--- lab_5/shoes_api/shoes/urls.py
+++ lab_5/shoes_api/shoes/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('create/', views.create_shoe, name='create'),
-    path('edit/<int:id>/', views.edit_shoe, name='edit'),
-    path('delete/<int:id>/', views.delete_shoe, name='delete'),
+    path('', views.shoes_list, name='index'),
+    path('create/', views.shoe_create, name='shoe_create'),
+    path('<int:pk>/edit/', views.shoe_edit, name='shoe_edit'),
+    path('<int:pk>/delete/', views.shoe_delete, name='shoe_delete'),
 ]
--- lab_5/shoes_api/shoes/views.py
+++ lab_5/shoes_api/shoes/views.py
@@ -1,35 +1,48 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.http import JsonResponse
 from .models import Shoe
 from .forms import ShoeForm
 
-def index(request):
+def shoes_list(request):
     shoes = Shoe.objects.all()
+
+    query = request.GET.get('q')
+    if query:
+        shoes = shoes.filter(producer__icontains=query)
+
+    sort = request.GET.get('sort')
+    if sort in ['producer', 'price', 'size', 'color']:
+        shoes = shoes.order_by(sort)
+
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        data = list(shoes.values())
+        return JsonResponse({'shoes': data})
+
     return render(request, 'shoes/index.html', {'shoes': shoes})
 
-def create_shoe(request):
-    if request.method == 'POST':
+
+def shoe_create(request):
+    if request.method == "POST":
         form = ShoeForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('index')
+            return redirect("index")
     else:
         form = ShoeForm()
-    return render(request, 'shoes/create.html', {'form': form})
+    return render(request, "shoes/create.html", {"form": form})
 
-def edit_shoe(request, id):
-    shoe = get_object_or_404(Shoe, id=id)
-    if request.method == 'POST':
+def shoe_edit(request, pk):
+    shoe = get_object_or_404(Shoe, pk=pk)
+    if request.method == "POST":
         form = ShoeForm(request.POST, instance=shoe)
         if form.is_valid():
             form.save()
-            return redirect('index')
+            return redirect("index")
     else:
         form = ShoeForm(instance=shoe)
-    return render(request, 'shoes/edit.html', {'form': form})
+    return render(request, "shoes/edit.html", {"form": form, "shoe": shoe})
 
-def delete_shoe(request, id):
-    shoe = get_object_or_404(Shoe, id=id)
-    if request.method == 'POST':
-        shoe.delete()
-        return redirect('index')
-    return render(request, 'shoes/delete.html', {'shoe': shoe})
+def shoe_delete(request, pk):
+    shoe = get_object_or_404(Shoe, pk=pk)
+    shoe.delete()
+    return redirect("index")
